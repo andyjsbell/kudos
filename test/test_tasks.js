d76fd26b29c63ce7dd63827d4e9572f2d54379d6
Andy Bell <andy.bell@barkingmad.io>
2019-11-26 15:36:49 +0100

    Initial tests for tasks

---
+++ test/test_tasks.js
@@ -0,0 +1,32 @@
+const Tasks = artifacts.require("./Tasks.sol");
+const KudosToken = artifacts.require("./KudosToken.sol");
+// const truffleAssert = require('truffle-assertions');
+const { toBN, stringToHex, toWei } = web3.utils;
+
+contract("Tasks", async accounts => {
+
+    let [owner, taskOwner, taskHunter, taskHunter1] = accounts; 
+    let kudosTokenInstance;
+    let tasksInstance;
+    let taskOwnerBalance, taskHunterBalance, taskHunter1Balance;
+    const initialTokenBalance = 100;
+
+    before("prepare some things", async function() {
+        
+        kudosTokenInstance = await KudosToken.deployed();
+        tasksInstance = await Tasks.deployed();
+
+        await kudosTokenInstance.transfer(taskOwner, initialTokenBalance, {from: owner});
+        await kudosTokenInstance.transfer(taskHunter, initialTokenBalance, {from: owner});
+        await kudosTokenInstance.transfer(taskHunter1, initialTokenBalance, {from: owner});
+        
+        taskOwnerBalance = await kudosTokenInstance.balanceOf(taskOwner);
+        taskHunterBalance = await kudosTokenInstance.balanceOf(taskHunter);
+        taskHunter1Balance = await kudosTokenInstance.balanceOf(taskHunter1);
+
+        assert.strictEqual(parseInt(taskOwnerBalance.toString()), initialTokenBalance);
+        assert.strictEqual(parseInt(taskHunterBalance.toString()), initialTokenBalance);
+        assert.strictEqual(parseInt(taskHunter1Balance.toString()), initialTokenBalance);
+    });
+  
+});

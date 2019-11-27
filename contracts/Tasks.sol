01ecdfed1f082302245c3bb4db7c9b9b1adf8475
Andy Bell <andy.bell@barkingmad.io>
2019-11-27 10:21:47 +0100

    document the class a little

--- contracts/Tasks.sol
+++ contracts/Tasks.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.5.0;
 import './KudosToken.sol';
 
+/// @title A list of Tasks
+/// @author Andy Bell andy.bell@displaynote.com
+/// @notice You can use this contract to create and complete tasks set
 contract Tasks {
 
     struct Task {
@@ -16,10 +19,20 @@ contract Tasks {
 
     KudosToken private kudos;
 
+    /// @author Andy Bell andy.bell@displaynote.com
+    /// @notice Create a task to be completed
+    /// @param _id A 32 character hash which would point to decentralised metainfo
+    /// @param _tokens A number of Kudos tokens for this task
+    /// @return boolean whether we succesfully transfer the tokens for the task
     constructor(KudosToken _kudos) public {
         kudos = _kudos;
     }
 
+    /// @author Andy Bell andy.bell@displaynote.com
+    /// @notice Create a task to be completed
+    /// @param _id A 32 character hash which would point to decentralised metainfo
+    /// @param _tokens A number of Kudos tokens for this task
+    /// @return boolean whether we succesfully transfer the tokens for the task
     function createTask(bytes32 _id, uint32 _tokens)
         public
         returns (bool) {
@@ -40,23 +53,31 @@ contract Tasks {
         return kudos.transferFrom(msg.sender, address(this), _tokens);
     }
 
+    /// @author Andy Bell andy.bell@displaynote.com
+    /// @notice Add caller as a hunter for the task
+    /// @param _id A 32 character hash which would point to the task
     function addHunter(bytes32 _id)
         public {
         require(_id[0] != 0, 'Invalid id');
         require(tasks[_id].owner != address(0x0), 'Task does not exist');
     }
 
-    function completeTask(bytes32 _id, address winner)
+    /// @author Andy Bell andy.bell@displaynote.com
+    /// @notice Complete a task
+    /// @dev Only the owner can complete a task
+    /// @param _id A 32 character hash which would point to the task
+    /// @param _winner Address of the hunter
+    function completeTask(bytes32 _id, address _winner)
         public {
         require(tasks[_id].owner == msg.sender, 'Invalid task');
         require(tasks[_id].hunters.length > 0, 'No hunters');
-        require(winner != address(0x0), 'Invalid hunter');
- 
+        require(_winner != address(0x0), 'Invalid hunter');
+
         uint len = tasks[_id].hunters.length;
         address payee = address(0x0);
         for (uint i = 0; i < len; i++) {
-            if (winner == tasks[_id].hunters[i]) {
-                payee = winner;
+            if (_winner == tasks[_id].hunters[i]) {
+                payee = _winner;
                 break;
             }
         }

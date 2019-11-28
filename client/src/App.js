e9de05c6910594e8acd523bd39945273d1659373
Andy Bell <andy.bell@barkingmad.io>
2019-11-28 12:50:01 +0100

    Bootstrap web3 and load our contracts

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
 import './App.css';
+import getWeb3 from "./utils/getWeb3";
+import 'semantic-ui-css/semantic.min.css';
+// import { Button, Input } from 'semantic-ui-react';
+import KudosToken from './contracts/KudosToken.json'
+import Tasks from './contracts/Tasks.json'
+// web3 https://github.com/ethereum/wiki/wiki/JavaScript-API
+const Web3 = require('web3');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {web3: null, accounts: [], kudos: null, tasks: null};
+  
+  handleChange(event, newValue) {
+    this.setState({value: newValue});
+  }
+
+  componentDidMount = async () => {
+    try {
+      // Workaround for compatibility between web3 and truffle-contract
+      Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send;	
+      
+      const web3 = await getWeb3();
+      const accounts = await web3.eth.accounts;
+      const networkId = await web3.version.network;
+      const kudosTokenDeployedNetwork = KudosToken.networks[networkId];
+      const tasksDeployedNetwork = Tasks.networks[networkId];
+      const kudosTokenContract  = web3.eth.contract(KudosToken.abi);
+      const kudosInstance = kudosTokenContract.at(kudosTokenDeployedNetwork.address);
+      const tasksContract  = web3.eth.contract(Tasks.abi);
+      const tasksInstance = tasksContract.at(tasksDeployedNetwork.address);
+
+      this.setState({ web3:web3, 
+                      accounts:accounts, 
+                      kudos:kudosInstance, 
+                      tasks:tasksInstance });
+      
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+
+      console.error(error);
+    }
+  };
+
+  render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contracts...</div>;
+    }
+    
+    return (
+      <>
+        <h1>Welcome to Kudos!</h1>
+      </>
+    );
+  }
 }
 
 export default App;

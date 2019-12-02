19c8f34cfb108712b4c74955d80288d838ea099f
Andy Bell <andy.bell@barkingmad.io>
2019-12-02 15:36:57 +0100

    convert ipfs hash to 32 bytes

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,32 @@ import 'semantic-ui-css/semantic.min.css';
 import { Table, Button, Input, Label, Form } from 'semantic-ui-react';
 import KudosToken from './contracts/KudosToken.json';
 import Tasks from './contracts/Tasks.json';
+import bs58 from 'bs58';
 const IPFS = require('ipfs');
+
+// Return bytes32 hex string from base58 encoded ipfs hash,
+// stripping leading 2 bytes from 34 byte IPFS hash
+// Assume IPFS defaults: function:0x12=sha2, size:0x20=256 bits
+// E.g. "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL" -->
+// "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
+
+function getBytes32FromIpfsHash(ipfsListing) {
+  return "0x"+bs58.decode(ipfsListing).slice(2).toString('hex')
+}
+
+// Return base58 encoded ipfs hash from bytes32 hex string,
+// E.g. "0x017dfd85d4f6cb4dcd715a88101f7b1f06cd1e009b2327a0809d01eb9c91f231"
+// --> "QmNSUYVKDSvPUnRLKmuxk9diJ6yS96r1TrAXzjTiBcCLAL"
+
+function getIpfsHashFromBytes32(bytes32Hex) {
+  // Add our default ipfs values for first 2 bytes:
+  // function:0x12=sha2, size:0x20=256 bits
+  // and cut off leading "0x"
+  const hashHex = "1220" + bytes32Hex.slice(2)
+  const hashBytes = Buffer.from(hashHex, 'hex');
+  const hashStr = bs58.encode(hashBytes)
+  return hashStr
+}
 
 const Wallet = (props) => {
   const [balance, setBalance] = useState('0');
@@ -132,7 +157,7 @@ const TaskEntry = (props) => {
   const [error, setError] = useState('');
   
   const createTask = () => {
-
+    
     props.kudos.allowance(account, props.tasks.address, {from: account}, (err, result) => {
       
       if (err) {
@@ -159,8 +184,7 @@ const TaskEntry = (props) => {
             
             setMessage('Task stored');
 
-            const id = result[0].hash;
-
+            const id = getBytes32FromIpfsHash(result[0].hash);
             props.tasks.createTask(id, kudos, {from: account}, (err, result) => {
             
               if(err) {

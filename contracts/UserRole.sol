9897c44e32a146ed920b24bf988570ff385a8ccc
Andy Bell <andy.bell@barkingmad.io>
2019-12-03 16:22:40 +0100

    only add users when not paused

--- contracts/UserRole.sol
+++ contracts/UserRole.sol
@@ -1,13 +1,15 @@
 pragma solidity ^0.5.0;
 
 import '@openzeppelin/contracts/ownership/Ownable.sol';
+import '@openzeppelin/contracts/lifecycle/Pausable.sol';
 
-contract UserRole is Ownable {
+contract UserRole is Ownable, Pausable  {
 
     mapping(address => bytes32) public users;
 
     function updateUser(bytes32 _id)
         public
+        whenNotPaused
         returns (bool) {
         require(_id[0] != 0, 'Invalid id');
         users[msg.sender] = _id;
